9d687f69ca82269c92a90be4b800e1e324af298a
Aladdin <alaaden3000@gmail.com>
2015-07-24 20:58:11 +0200

    Add New Page cPanel

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/cPanel', function(req, res) {
+  res.render('cPanel', { title: 'Control Panel' });
+});
+
 module.exports = router;
