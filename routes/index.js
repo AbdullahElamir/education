ece84ecb8e1f54dad9bb862d62e7fd3b858dc317
Aladdin <alaaden3000@gmail.com>
2015-07-29 20:52:55 +0200

    add locations and semesters viwes

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,28 @@ router.get('/cPanel', function(req, res) {
   res.render('cPanel', { title: 'Control Panel' });
 });
 
+router.get('/semesters', function(req, res) {
+  res.render('semesters', { title: 'View Semesters' });
+});
+
+router.get('/newSemester', function(req, res) {
+  res.render('newSemester', { title: 'New Semester' });
+});
+
+router.get('/locations', function(req, res) {
+  res.render('locations', { title: 'View Locations' });
+});
+
+router.get('/newLocation', function(req, res) {
+  res.render('newLocation', { title: 'New Location' });
+});
+
+router.get('/departments', function(req, res) {
+  res.render('departments', { title: 'View departments' });
+});
+
+router.get('/newDepartment', function(req, res) {
+  res.render('newDepartment', { title: 'New Department' });
+});
+
 module.exports = router;
