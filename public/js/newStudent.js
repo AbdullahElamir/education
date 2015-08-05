3f58a7f85508790bbb3722a3a01933055b3b53dd
Aladdin <alaaden3000@gmail.com>
2015-08-05 20:26:09 +0200

    cahnge design

--- public/js/newStudent.js
+++ public/js/newStudent.js
@@ -45,6 +45,50 @@ $(document).ready(function(){
       nationality:{
         required: true,
       },
+      gender:{
+        required: true,
+      },
+      no_paper_family:{
+        required: true,
+        number: true,
+      },
+      no_reg_family:{
+        required: true,
+        number: true,
+      },
+      physical_address:{
+        required: true,
+      },
+      civil_reg:{
+        required: true,
+      },
+      phone:{
+        required: true,
+      },
+      father_work_place:{
+        required: true,
+      },
+      nid:{
+        required: true,
+      },
+      last_cert:{
+        required: true,
+      },
+      cust_last_cert:{
+        required: true,
+      },
+      date_cert:{
+        required: true,
+      },
+      place_cert:{
+        required: true,
+      },
+      set_number:{
+        required: true,
+      },
+      student_rate:{
+        required: true,
+      },
     },
     messages:{
       first_name:{
@@ -52,6 +96,70 @@ $(document).ready(function(){
       },
       father_name:{
         required: "الرجاء اختيار اسم اﻷب",
+      },
+      grand_name:{
+        required: "الرجاء ادخال اسم الجد",
+      },
+      last_name:{
+        required: "الرجاء ادخال اللقب",
+      },
+      mother_name:{
+        required: "الرجاء ادخال اسم اﻷم",
+      },
+      place_birth:{
+        required: "الرجاء ادخال مكان الميلاد",
+      },
+      birth_date:{
+        required: "الرجاء ادخال سنة الميلاد",
+        number: "خطأ سنة الميلاد غير صحيح",
+        // rangelength: [1990, 2010],
+      },
+      nationality:{
+        required: "الرجاء اختيار الجنسية",
+      },
+      gender:{
+        required: "الرجاء اختيرا نوع الجنس",
+      },
+      no_paper_family:{
+        required: "الرجاء ادخال رقم ورقة العائلة",
+        number: "الرجاء ادخال رقم صحيح",
+      },
+      no_reg_family:{
+        required: "الرجاء ادخال رقم قيد العائلة",
+        number: "الرجاء ادخال رقم صحيح",
+      },
+      physical_address:{
+        required: "الرجاء ادخال عنوان الاقامة",
+      },
+      civil_reg:{
+        required: "الرجاء ادخال السجل المدني",
+      },
+      phone:{
+        required: "الرجاء ادخال رقم الهاتف",
+      },
+      father_work_place:{
+        required: "الرجاء ادخال مكان عمل اﻷب",
+      },
+      nid:{
+        required: "الرجاء ادخال الرقم الوطني",
+      },
+      last_cert:{
+        required: "الرجاء ادخال اخر شهادة",
+      },
+      cust_last_cert:{
+        required: "الرجاء ادخال تخصيص اخر شهادة",
+      },
+      date_cert:{
+        required: "الرجاء ادخال تاريخ الحصول علي الشهادة",
+      },
+      place_cert:{
+        required: "الرجاء ادخال مكان الحصول علي الشهادة",
+      },
+      set_number:{
+        required: "الرجاء ادخال رقم الجلوس",
+      },
+      student_rate:{
+        required: "الرجاء ادخال معدل الطالب/ة",
       },
     },
     // errorElement: 'label',
