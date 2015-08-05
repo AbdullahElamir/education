1ac9c042c904f22af3d2c4b64db642136f92c749
Mohamed Elgmati <mohamed199188@gmail.com>
2015-08-05 19:54:27 +0200

    addtable

---
+++ models/timeline.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Timeline = sequelize.define("Timeline", {
+    starting_time:{ type: DataTypes.TIME(),defaultValue: null},
+    day: DataTypes.INTEGER(1),
+    ending_time:{ type: DataTypes.TIME(),defaultValue: null}
+
+  },{
+    classMethods: {
+      associate: function(models) {
+        Timeline.belongsTo(models.Subject, {
+          onDelete: "CASCADE",
+          foreignKey: {
+            allowNull: false
+          }
+        });
+        Timeline.belongsTo(models.Location, {
+          onDelete: "CASCADE",
+          foreignKey: {
+            allowNull: false
+          }
+        });
+        Timeline.belongsTo(models.User, {
+          onDelete: "CASCADE",
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      }
+    }
+  });
+
+  return Timeline;
+};
