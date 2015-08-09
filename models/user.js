cdc92f26b9b97db95776eb6e77a51f40bb7494f7
FaisalRahil <faisallrahil@gmail.com>
2015-08-09 18:45:14 +0200

    edit

--- models/user.js
+++ models/user.js
@@ -2,7 +2,12 @@
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    username: DataTypes.STRING
+    name: DataTypes.STRING(150),
+    email: DataTypes.STRING(150),
+    password: DataTypes.STRING(150),
+    salt: DataTypes.STRING(150),
+    phone: DataTypes.STRING(150),
+    status :{type: DataTypes.INTEGER(150),defaultValue:1}
   }, {
     classMethods: {
       associate: function(models) {
