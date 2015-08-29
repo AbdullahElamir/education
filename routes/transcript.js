10c21982840fe5381bbd9ee61df191c037fe93ba
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2015-08-29 12:00:11 +0200

    subject endintion

--- routes/transcript.js
+++ routes/transcript.js
@@ -106,7 +106,7 @@ router.get('/studentData/:id',userHelpers.isLogin, function(req, res) {
          models.SemesterStudent.findAll({
           where: {
           status: 1,
-          id: req.params.id
+          StudentId: req.params.id
           },
       "include" : [
         {"model" : models.Division},
