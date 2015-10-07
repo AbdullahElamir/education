d7ebd97f976a850f52cf08d41752e93e83dca095
Aladdin Sonni <alaaden3000@gmail.com>
2015-10-07 19:25:30 +0200

    #80

--- routes/report.js
+++ routes/report.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var models = require('../models');
 var login = require('../app/login')(router);
 var userHelpers = require('../app/userHelpers');
-var jsr = require("jsreport");
+var jsreport = require("jsreport");
 var fs = require("fs");
 var path = require("path");
 
@@ -17,7 +17,7 @@ router.get('/', userHelpers.isLogin, function (req, res) {
 });
 
 router.get('/presenceAbsenceSubject', userHelpers.isLogin, function (req, res, next) {
-  jsr.render({
+  jsreport.render({
     template: {
       content: fs.readFileSync(path.join(__dirname, "../views/presenceAbsenceSubject.html"), "utf8"),
       recipe: "phantom-pdf",
@@ -28,9 +28,9 @@ router.get('/presenceAbsenceSubject', userHelpers.isLogin, function (req, res, n
 });
 
 router.get('/PresenceAbsenceLectures', userHelpers.isLogin, function (req, res, next) {
-  jsr.render({
+  jsreport.render({
     template: {
-      content: fs.readFileSync(path.join(__dirname, "../views/PresenceAbsenceLectures.html"), "utf8"),
+      content: fs.readFileSync(path.join(__dirname, "../views/presenceAbsenceLectures.html"), "utf8"),
       recipe: "phantom-pdf",
     },
   }).then(function (response) {
