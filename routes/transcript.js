264de4d5d27a05099381a43cbda31f8631cd29c6
abdooageel <ageel.abdoo@gmail.com>
2015-12-20 08:30:49 +0200

    fixed downlode

--- routes/transcript.js
+++ routes/transcript.js
@@ -1902,6 +1902,10 @@ router.post('/addSemesterStudent', userHelpers.isLogin, function (req, res) {
                         order: '`id` DESC',
                         limit:2,
                       }).then(function(semesterid){
+                        if(semesterid[1]==undefined){
+                          semesterid[1]=[];
+                          semesterid[1]['id']=-99;
+                        }
                         models.Academic_transcript.findAll({
                           where:{
                             SemesterStudentId:semesterid[1].id,
@@ -1949,7 +1953,9 @@ router.post('/addSemesterStudent', userHelpers.isLogin, function (req, res) {
                                
                               }
                               models.Academic_transcript.bulkCreate(listF);
-                            });  
+                            }); 
+
+
                           }
                           var list = [];
                       
