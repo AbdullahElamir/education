5bd5573c7e546a44794528cdcd9167648fa55b3e
MohamedAlgmate <mohamed199188@gmail.com>
2016-04-23 19:26:29 +0200

    edit msg student

--- public/js/newStudent.js
+++ public/js/newStudent.js
@@ -157,4 +157,23 @@ $(document).ready(function(){
     else
       e.preventDefault();
   });
+  var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+      var p=a[i].split('=', 2);
+      if (p.length == 1)
+        b[p[0]] = "";
+      else
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+  })(window.location.search.substr(1).split('&'));
+  
+if(qs["msg"]==2){
+    custNotify("danger","خطأ","رقم القيد موجود مسبقا","ok-sign","bounceInDown","bounceOutUp");
+    var pageUrl = '/student/newStudent'
+    window.history.pushState("","",pageUrl);
+  }
 });
--- public/js/student.js
+++ public/js/student.js
@@ -129,4 +129,23 @@ $(document).ready(function(){
     }
     return false;
   });
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+      var p=a[i].split('=', 2);
+      if (p.length == 1)
+        b[p[0]] = "";
+      else
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+  })(window.location.search.substr(1).split('&'));
+  
+if(qs["msg"]==1){
+    custNotify("success","نجح","تمت تسجيل طالب/ة جديد بنجاح","ok-sign","bounceInDown","bounceOutUp");
+    var pageUrl = '/student'
+    window.history.pushState("","",pageUrl);
+  }
 });
